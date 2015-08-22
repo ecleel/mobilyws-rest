5b6b40dc8210128a5a13e7cba8066c8a74d9fb6d
Abdulaziz AlShetwi <ecleeld@gmail.com>
2015-08-22 04:14:30 +0300

    First release

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+ruby "2.2.2"
+
+gem 'cuba'
+gem 'mobilyws'
+gem 'puma'
+gem 'dotenv'
+
+gem 'shotgun', require: false, group: :development
---
+++ app.rb
@@ -0,0 +1,43 @@
+require "json"
+require "mobilyws"
+require "cuba"
+
+sms = Mobilyws::API.new ENV["USERNAME"], ENV["PASSWORD"], ENV["SENDER_NAME"]
+message = ENV["MESSAGE"]
+
+# TODO user can send new message.
+
+auth_token = ENV["AUTH_TOKEN"]
+
+Cuba.define do
+  # set headers
+  res.headers["Content-Type"] = "application/json; charset=utf-8"
+
+  on post do
+    
+    user_token = env["HTTP_AUTHENTICATION"]
+    on auth_token.eql?(user_token) do
+
+      on "send" do
+        on root do
+          on param("mobile") do |mobile|
+            name = req.params.fetch('name', '')
+            message = message % [name]
+            response = sms.send(message: message, numbers: [mobile])
+            response_message  = {status: :sent, message: response}.to_json
+            res.write response_message
+          end
+
+          # If the params `mobile` is not provided, this
+          # block will get executed.
+          on true do
+            error_message = {status: :error, message: "mobile number missing"}.to_json
+            res.write error_message
+          end
+        end
+        
+      end
+      
+    end
+  end
+end
